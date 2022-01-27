2fce087d1108802e7769256094f6504f2a74acbe
Daniel Soliwoda <zemial85@gmail.com>
2022-01-27 11:55:01 +0100

    Initial commit

---
+++ Handwriting.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.text_to_handwriting('''Hello, My name is Daniel Soliwoda. Learning to become python developer. ''')
---
+++ main.py
@@ -0,0 +1,9 @@
+from faker import Faker
+
+fake = Faker()
+
+print(fake.name())
+print(fake.email())
+print(fake.country())
+
+print(fake.profile())
